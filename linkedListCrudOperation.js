efdf42acc65aadc00b34e1c2832bbdbe844be9bd
PallaviKhedle <pallavi8khedle@gmail.com>
2023-03-06 16:55:52 -0800

    first commit

---
+++ linkedListCrudOperation.js
@@ -0,0 +1,9 @@
+// Basic Operations
+// Following are the basic operations supported by a list.
+// 1. Insertion − Adds an element at the beginning of the list.
+// 2. Deletion − Deletes an element at the beginning of the list.
+// 3. Display − Displays the complete list.
+// 4. Search − Searches an element using the given key.
+// 5. Delete − Deletes an element using the given key.
+
+
