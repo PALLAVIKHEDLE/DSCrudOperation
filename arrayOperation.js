35adc9984961a95c7e2ec633831349d5ec7e57e7
PallaviKhedle <pallavi8khedle@gmail.com>
2023-03-13 12:09:53 -0700

    array operation

---
+++ arrayOperation.js
@@ -0,0 +1,26 @@
+//An array data structure or an array is an ordered list of values,
+//or a collection of elements (values or variables) identified by an index or key.
+//The most simple type of array data structure is a linear array.
+
+
+1. // Create an array
+// Creating an array literal will require you to write a pair of square brackets.
+
+Example:
+const myArray = [ ];
+
+// We can add a constructor function
+// const myArray = new Array( );
+
+
+2. //Access a specific value in array
+myArray [0];// pass a index
+
+3. //adding a value in specific index of array 
+myArray[0] = "Superman"
+
+4. 
+// The length property of an array is defined as the number of elements it contains.
+const arr = ['a', 'b', 'c', 'd']
+console.log(arr.length) // 4
+
