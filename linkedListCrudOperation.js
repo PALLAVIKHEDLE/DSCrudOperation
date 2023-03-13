fc3ddc34b903e309fba0c5626a4c434185d6afd9
PallaviKhedle <pallavi8khedle@gmail.com>
2023-03-13 12:09:13 -0700

    reverse linked list

--- linkedListCrudOperation.js
+++ linkedListCrudOperation.js
@@ -218,3 +218,24 @@ reverse()
   }
   return this
 }
+
+// O(n) time & O(1) space
+function reverse(head) {
+    let node = head,
+        previous,
+        tmp;
+  
+    while (node) {
+      // save next before we overwrite node.next!
+      tmp = node.next;
+  
+      // reverse pointer
+      node.next = previous;
+  
+      // step forward in the list
+      previous = node;
+      node = tmp;
+    }
+  
+    return previous;
+  }
